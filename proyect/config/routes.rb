0be94e1964944d0d71a627f0dae1ac9a389a09bd
ppariztia1 <ppariztia1@miuandes.cl>
2025-10-28 22:43:22 -0300

    ready entrega 3

--- proyect/config/routes.rb
+++ proyect/config/routes.rb
@@ -5,15 +5,13 @@ Rails.application.routes.draw do
   get "user_badges/show"
   root "challenges#index"
 
-  resources :challenges,      only: [:index, :show]
-  resources :users,           only: [:index, :show]
-  resources :badges,          only: [:index, :show]
-  resources :tags,            only: [:index, :show]
-  resources :participations,  only: [:index, :show]
-  resources :progress_entries,only: [:index, :show]
+  resources :challenges
+  resources :users
+  resources :badges
+  resources :tags
+  resources :participations
+  resources :progress_entries
   resources :notifications,   only: [:index, :show]
   resources :user_badges,     only: [:index, :show]
   resources :challenge_tags,  only: [:index, :show]
 end
-
-
